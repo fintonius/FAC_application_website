28864a7da4e438a08da79123578978383be1f878
fintonius <fintonius.mcginty@gmail.com>
2023-07-11 20:49:15 +0100

    trying to get layout right!

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const artLink = document.getElementsByClassName('art-link');
-const webLink = document.getElementsByClassName('web-link');
+let art = document.getElementById('art');
+art.addEventListener('click', () => {window.location.replace("art-link.html")})
 
+let home = document.getElementById('home');
+home.addEventListener('click', homePage);
 
+function homePage() {
+    window.location.replace("index.html");
+    console.log('click!');
+}
+
+let web = document.getElementById('web');
+web.addEventListener('click', () => {window.location.replace("web-link.html")})
